a77445d53a0c0a2cd9cb7a024349cbb31060c64b
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-05-07 14:03:05 +0300

    başlangıc 1

--- src/main/java/com/HelloWork/helloWork/controller/HelloController.java
+++ src/main/java/com/HelloWork/helloWork/controller/HelloController.java
@@ -1,21 +1,48 @@
 package com.HelloWork.helloWork.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api")
 public class HelloController {
 
+    /*
+    Get   : Veri görüntülemek istediğimiz zaman görüntülüyoruz.               = @GetMapping
+    Post  : veri kaydetmek istediğimiz zaman kullanıyoruz.                    = @PostMapping
+    Put   : Veri güncellemek için kullanıyoruz.                               = @PutMapping
+    Patch : Verilerin bir bölümünü güncellemek istediğimiz zaman kullanırız.  =  @PatchMapping
+          ÖR: parola güncelemek gibi
+   Delete : veri silmek istediğimiz zaman kullanırız.                         =  @DeleteMapping
+     */
     // localHost:8080/hello/api
 
-     @GetMapping(path = "/Hello")
+    @GetMapping(path = "/Hello")
     // @RequestMapping(path = "/hello", method = RequestMethod.GET) = üsteki ile aynı işlemi görüyor
     public String sayHello() {
         return "Hello world";
     }
+    @GetMapping(path = "view")
+    public String view(){
+        return "görüntüleme";
+    }
+    @PostMapping(path ="save" )
+    public String save() {
+        return "Kaydetme";
+    }
+    @PostMapping(path ="update")
+    public String update() {
+        return "Güncelleme";
+    }
+    @PatchMapping(path ="update_a_part")
+    public String update_A_Part() {
+        return "bir bölümünü güncellemek";
+    }
+    @DeleteMapping(path = "to_dalete")
+    public String to_Delete() {
+        return "silmek";
+    }
+
+
 }
 
 
