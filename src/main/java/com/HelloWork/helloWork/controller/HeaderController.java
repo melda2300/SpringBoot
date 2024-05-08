cd85594d66e9fc7aca393f0a6b79ba6f4faff577
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-05-08 20:27:12 +0300

    yeni dosya yüklendi

---
+++ src/main/java/com/HelloWork/helloWork/controller/HeaderController.java
@@ -0,0 +1,20 @@
+package com.HelloWork.helloWork.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/Header")
+public class HeaderController {
+
+
+    // localHost:8080/Header/header
+
+    @GetMapping(path = "/header")
+    public String getHeader( @RequestHeader("My-Header") String myHeader ){
+        return "Your hader is : "+myHeader;
+    }
+}
